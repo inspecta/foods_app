8654e11f9cf392821084150bbd5f77d5ebc3a3b8
Derrick Mulinde <djmulinde@gmail.com>
2023-02-11 17:37:07 +0300

    Update views and styles for navigation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,8 +1,14 @@
 class Food < ApplicationRecord
+
   belongs_to :user, foreign_key: :user_id
 
   validates :name, presence: true
   validates :quantity, presence: true
   validates :price, presence: true
   validates :measurement_unit, presence: true
+
+  # Sum prices
+  def self.total_price
+    sum(:price)
+  end
 end
