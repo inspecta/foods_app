14d5c7ad663ca975ea72ffd2350a66b48d9f97a0
Derrick Mulinde <djmulinde@gmail.com>
2023-02-11 19:34:51 +0300

    Add spec folder

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'Microverse', email: 'microverse23@gmail.com',
+  password: 'password'
+  ) }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid with invalid attributes" do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "shoud have an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "should have a password" do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+end
