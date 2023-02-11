9fbbf1ee385ed464d857fed3c9d9bae5abf06fe9
Derrick Mulinde <djmulinde@gmail.com>
2023-02-11 07:01:20 +0300

    Add more routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root "users#index"
   resources :users, only: [:index, :show] do
+    resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    end
   end
+
+  resources :public_recipes, only: [:index]
 end
